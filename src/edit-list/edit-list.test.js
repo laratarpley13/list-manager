55c728605c2331a8a74392eeac062517a8af0813
laratarpley13 <ltarp13@gmail.com>
2021-03-24 17:41:46 -0500

    added test files

--- src/edit-list/edit-list.test.js
+++ src/edit-list/edit-list.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import EditList from './edit-list';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
   ReactDOM.render(
     <BrowserRouter>
-      <App />
+      <EditList />
     </BrowserRouter>,
     div
   );
--- src/sign-in/sign-in.test.js
+++ src/sign-in/sign-in.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import SignIn from './sign-in';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
 
   ReactDOM.render(
     <BrowserRouter>
-      <App />
+      <SignIn />
     </BrowserRouter>,
     div
   );
